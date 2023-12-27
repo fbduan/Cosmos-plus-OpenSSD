4e77ad9747f2b5dd9c789a992ed73cd353873a8a
fbduan <fbduan@hotmail.com>
2023-12-27 18:27:03 +0800

    add ucode parsing tool

---
+++ source/work/tools/ucode_parsing.py
@@ -0,0 +1,70 @@
+#!/bin/env python3
+
+import os
+import sys
+import math
+
+import prettytable
+
+from collections import OrderedDict as odict
+
+uPFunc_bits          = 4
+uPLength_bits        = 16
+uPTargetID_bits      = 5
+uPSourceID_bits      = 5
+uPOpcode_bits        = 6
+uPAddrSrcRegSel_bits = 4
+uPChSelect_bits      = 1
+
+uPFunc = {}
+uPFunc[1] = 'WritecmdToModule'
+uPFunc[2] = 'ReadcmdToModule '
+uPFunc[4] = 'UpTrans         '
+uPFunc[8] = 'DownTrans       '
+for i in range(2**uPFunc_bits):
+    if i not in uPFunc:
+        uPFunc[i] = "*"
+
+uPChSelect = {}
+uPChSelect[0] = "Datapath   "
+uPChSelect[1] = "V2NFC100DDR"
+
+uPAddrSrcRegSel = {}
+uPAddrSrcRegSel[0] = "32'h0000_0000"
+uPAddrSrcRegSel[1] = 'RowAddress   '
+uPAddrSrcRegSel[2] = 'UserData     '
+uPAddrSrcRegSel[3] = 'DataAddress  '
+uPAddrSrcRegSel[4] = 'SpareAddress '
+uPAddrSrcRegSel[5] = 'ErrCntAddress'
+uPAddrSrcRegSel[6] = 'CmpltAddress '
+uPAddrSrcRegSel[7] = 'WaySelection '
+uPAddrSrcRegSel[8] = 'ColAddress   '
+for i in range(2**uPAddrSrcRegSel_bits):
+    if i not in uPAddrSrcRegSel:
+        uPAddrSrcRegSel[i] = "*"
+
+
+mif_file = sys.argv[1]
+print(f"{mif_file=}")
+
+mif = []
+mif_tab = prettytable.PrettyTable()
+mif_tab.field_names = ['Index', 'uPChSelect', 'uPAddrSrcRegSel', 'uPOpcode', 'uPSourceID', 'uPTargetID', 'uPLength', 'uPFunc']
+with open(mif_file, "r") as fp:
+    for l in fp:
+        l = l.strip()
+        mif.append( \
+                { \
+                 'func'  : int(l[-1*(uPFunc_bits                                                                                                 ):                                                                                                 ], 2),
+                 'len'   : int(l[-1*(uPFunc_bits+uPLength_bits                                                                                   ):-1*(uPFunc_bits                                                                                 )], 2),
+                 'tgtid' : int(l[-1*(uPFunc_bits+uPLength_bits+uPTargetID_bits                                                                   ):-1*(uPFunc_bits+uPLength_bits                                                                   )], 2),
+                 'srcid' : int(l[-1*(uPFunc_bits+uPLength_bits+uPTargetID_bits+uPSourceID_bits                                                   ):-1*(uPFunc_bits+uPLength_bits+uPTargetID_bits                                                   )], 2),
+                 'opcode': int(l[-1*(uPFunc_bits+uPLength_bits+uPTargetID_bits+uPSourceID_bits+uPOpcode_bits                                     ):-1*(uPFunc_bits+uPLength_bits+uPTargetID_bits+uPSourceID_bits                                   )], 2),
+                 'regsel': int(l[-1*(uPFunc_bits+uPLength_bits+uPTargetID_bits+uPSourceID_bits+uPOpcode_bits+uPAddrSrcRegSel_bits                ):-1*(uPFunc_bits+uPLength_bits+uPTargetID_bits+uPSourceID_bits+uPOpcode_bits                     )], 2),
+                 'chsel' : int(l[-1*(uPFunc_bits+uPLength_bits+uPTargetID_bits+uPSourceID_bits+uPOpcode_bits+uPAddrSrcRegSel_bits+uPChSelect_bits):-1*(uPFunc_bits+uPLength_bits+uPTargetID_bits+uPSourceID_bits+uPOpcode_bits+uPAddrSrcRegSel_bits)], 2),
+                }
+        )
+        mif_tab.add_row(["%03x"%(len(mif)), "%01x-%s"%(mif[-1]['chsel'], uPChSelect[mif[-1]['chsel']]), "%01x-%s"%(mif[-1]['regsel'], uPAddrSrcRegSel[mif[-1]['regsel']]), "%02x"%mif[-1]['opcode'], "%02x"%mif[-1]['srcid'], "%02x"%mif[-1]['tgtid'], "%04x"%mif[-1]['len'], "%01x-%s"%(mif[-1]['func'], uPFunc[mif[-1]['func']])])
+
+print( "Info of %s:\n%s"%(mif_file, mif_tab.get_string()) )
+    
